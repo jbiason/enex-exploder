133d4f679298e61fe4c51b69d21a722f58a0c0bf
Julio Biason <julio.biason@deliverit.com.br>
2019-02-25 12:48:40 -0300

    Skeleton, initial parsing

---
+++ src/main.rs
@@ -0,0 +1,36 @@
+use std::env;
+use xml::{Event, Parser};
+use std::fs::File;
+use std::io::prelude::*;
+
+fn main() {
+    let args:Vec<_> = env::args().collect();
+    if args.len() != 2 {
+        println!("Required: filename.");
+        std::process::exit(2);
+    }
+    
+    let filename = &args[1];
+        
+    println!("Will process {}", filename);
+    
+    // Need to find a way to load small chunks and feed it to the parser while parsing.
+    // (E.g., load 1024 bytes, feed it to the parser and, if the parser can't continue,
+    //  feed more data, till the end of file).
+    let mut file = File::open(filename).unwrap();
+    let mut contents = String::new();
+    file.read_to_string(&mut contents).unwrap();
+        
+    let mut parser = Parser::new();
+    parser.feed_str(&contents);
+    
+    for event in parser {
+        match event.unwrap() {
+            Event::ElementStart(tag) => println!("Start: {}", tag.name),
+            Event::ElementEnd(tag) => println!("End: {}", tag.name),
+            Event::Characters(data) => println!("Data: {}", data),
+            Event::CDATA(data) => println!("CDATA: {}", data),
+            _ => ()
+        }
+    }
+}
